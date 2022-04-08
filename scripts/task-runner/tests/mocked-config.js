817c2523484b491c25914a076ace19efde592bd2
osamamammar <osamaammar29@gmail.com>
2022-04-08 21:41:52 +0200

    initial dist subtree commit

--- scripts/task-runner/tests/mocked-config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const { config } = require('../../config');
-
-module.exports = {
-	srcPath: path.join(__dirname, `${config.path.src}`),
-	distPath: path.join(__dirname, `${config.path.dist}`),
-};
